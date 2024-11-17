00f22214967f709f645025e2dae2082949c3bf7d
k6ivupuu <kaarel@koivupuu.ee>
2024-11-17 23:18:23 +0200

    Added basic placement for signup page and adding a post

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import {createRouter, createWebHistory } from 'vue-router'
 import HomePage from "@/views/HomePage.vue";
+import SignUp from "@/views/SignUp.vue";
+import AddPost from "@/views/AddPost.vue";
 
 const routes = [
     {
         path: "/",
         name: "Home",
         component: HomePage
+    },
+    {
+        path: "/login.html",
+        name: "Sign Up",
+        component: SignUp
+    },
+    {
+        path: "/addPost.html",
+        name: "Add Post",
+        component: AddPost
     }
 ]
 
---
+++ src/views/SignUp.vue
@@ -0,0 +1,32 @@
+<!-- src/views/HomePage.vue -->
+
+<template>
+  <div>
+    <h1>Signup functionality to this page</h1>
+    <div class="content">
+      <p>This is content placeholder</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+  components: {},
+  data() {
+    return {
+      message: "Hello World!"
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+.content {
+  display: flex;
+  flex: 1;
+}
+
+</style>
