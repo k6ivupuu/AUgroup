f45ee68ae11aec9b30f28b755eb4ee953cd0e108
k6ivupuu <kaarel@koivupuu.ee>
2024-11-19 21:30:55 +0200

    Added vuex.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router";
+import store from "@/store";
 import './assets/styles/variables.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 
 app.mount('#app')
